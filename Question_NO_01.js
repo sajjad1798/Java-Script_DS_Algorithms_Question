a93cf5cd46e3fae021a9e736a6cc310109ad6781
Sajjad <muhammadsajjad1798@gmail.com>
2021-07-04 20:49:27 -0700

    checking sum zero

---
+++ Question_NO_01.js
@@ -0,0 +1,17 @@
+// Checking sum zero - problem 1
+//  [-5,-4,-3,-2,0,2,4,6,8]
+// [-4 , 4] => output 
+
+const getSumPairZero = (array) =>{
+    for (let number of array){
+        for(let j=1; j<array.length; j++){
+            if(number + array[j] === 0){
+                return [number , array[j]];
+            }
+        }
+    }
+}
+const result = getSumPairZero([-5,-4,-3,-2,0,2,4,6,8]);
+console.log(result);
+
+// o(n^2) quadratic time complexity 
